b61c6b8d509e867073192b2e23295c4b8617d2e2
yana.podskrebalina <yanastealth@gmail.com>
2020-09-21 12:14:15 +0300

    Lesson 2 AQA- HeaderPromUaTest corrected

--- src/test/java/io/ctdev/tests/login/HeaderPromUaTest.java
+++ src/test/java/io/ctdev/tests/login/HeaderPromUaTest.java
@@ -1,5 +1,4 @@
 package io.ctdev.tests.login;
-
 import io.ctdev.tests.signup.BaseTest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,30 +8,25 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class HeaderCheck extends BaseTest {
+public class HeaderPromUaTest extends BaseTest {
     WebDriver driver;
 
     @Test
-    public void shouldAnswerWithTrue1() throws InterruptedException {
-
+    public void titleVerification() throws InterruptedException {
         // System.setProperty("webdriver.chrome.driver", "C://Program Files//WEBDrivers//chromedriver.exe");
-
         driver.get("http://prom.ua");
         String expectedTitle = "Prom.ua — маркетплейс Украины";
         String actualTitle = driver.getTitle();
         Thread.sleep(3000);
-        Assert.assertEquals(actualTitle, expectedTitle, "The title is not Google");
-
+        Assert.assertEquals(actualTitle, expectedTitle, "The title is not:Prom.ua — маркетплейс Украины");
     }
 
     @BeforeClass
     public void beforeClass() {
         System.setProperty("webdriver.chrome.driver", "C://Program Files//WEBDrivers//chromedriver.exe"); //настройки выносим в beforeClass()
-
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-fullscreen"); //используем options открыть full screen браузера
         options.setHeadless(true); //запустить браузер в headless режиме, мы браузер не видим
-
         driver = new ChromeDriver(options);
     }
 
