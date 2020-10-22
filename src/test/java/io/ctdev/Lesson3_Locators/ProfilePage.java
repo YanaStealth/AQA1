78860edf0889b293d88171e30056ac511070ce4f
yana.podskrebalina <yanastealth@gmail.com>
2020-10-22 22:15:02 +0300

    Lesson 3 AQA- Find by Css, Xpath Locators in Buj User Form

--- src/test/java/io/ctdev/Lesson3_Locators/ProfilePage.java
+++ src/test/java/io/ctdev/Lesson3_Locators/ProfilePage.java
@@ -11,9 +11,7 @@ public class ProfilePage {
     private String positionXpath="//input[@name='position']";
     private String departmentCss="[name=department]";
     private String departmentXpath="//input[@name='department']";
-    private String cancelCss="[.button_btn__2GBfK.editUserProfile_cancelBtn__fnKLZ]";
-    private String cancelXpath="//button[@type='button' and contains(., 'Cancel')]";
-    private String cancelButtonCss="[.button_btn__2GBfK.editUserProfile_cancelBtn__fnKLZ]";
+    private String cancelButtonCss=" [class*='editUserProfile_cancelBtn']";
     private String cancelButtonXpath="//button[@type='button' and contains(., 'Cancel')]";
     private String saveChangesCss="[type=submit]";
     private String saveChangesXpath="//button[@type='submit']";
