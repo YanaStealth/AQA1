897caf89a3a7c98cd1b93035fcc419f5a97b5802
yana.podskrebalina <yanastealth@gmail.com>
2020-10-31 00:22:03 +0200

    Lesson 5 AQA- Task 1 - Replace Threadsleep by Explicit wait

---
+++ src/test/java/io/ctdev/tests/login/ExplicitWait/LoginToJuiceShopNegativeExplicit.java
@@ -0,0 +1,97 @@
+package io.ctdev.tests.login.ExplicitWait;
+
+import io.ctdev.tests.framework.driver.WebDriverSingleton;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.ctdev.tests.framework.driver.WebDriverSingleton.getDriver;
+
+public class LoginToJuiceShopNegativeExplicit {
+
+        private String invalidUserName1 = "yana";
+        private String password = "qQ2$4";
+        WebDriver driver = getDriver(); //explicit wait
+        WebDriverWait wait; //explicit wait
+
+
+        @BeforeClass
+        public void setUp() throws InterruptedException {
+            // getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            wait= new WebDriverWait(driver, 500);
+            getDriver().get("http://3.18.213.48/");
+
+            getDriver().findElement(By.cssSelector("[class*='close-dialog']")).click();
+
+            System.out.println("Clicking on Account button");
+            WebElement element = getDriver().findElement(By.id("navbarAccount")); // можем либо создать объект элемента, либо напрямую его вызвать getDriver().
+            element.click();
+
+            System.out.println("Clicking on Login button");
+            getDriver().findElement(By.id("navbarLoginButton")).click();
+        }
+
+        @AfterClass
+        public void tearDown() {
+            WebDriverSingleton.closeDriver();
+        }
+
+
+        @Test (description = "Login -Negative Case - Login with empty email field")
+        public void loginEmptyEmailFieldValidationNegativeCase() throws InterruptedException {
+
+            System.out.println("Empty email field verification");
+            getDriver().findElement(By.id("email")).click();
+            getDriver().findElement(By.id("password")).click();
+            //Thread.sleep(2000);
+
+            WebElement emptyEmailFieldNotificationElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(text(),'Please provide an email address')]"))); //explicit wait
+            String emptyEmailFieldNotification = emptyEmailFieldNotificationElement.getAttribute("innerText").trim(); //explicit wait
+
+            //String emptyEmailFieldNotification = getDriver().findElement(By.xpath("//*[contains(text(),'Please provide an email address')]")).getAttribute("innerText").trim();
+            Assert.assertEquals(emptyEmailFieldNotification, "Please provide an email address.", "Email field does not have validation for empty value");
+
+        }
+
+        @Test(description = "Login - Negative Case - Login with empty password field")
+        public void loginEmptyPasswordFieldValidationNegativeCase() throws InterruptedException {
+
+            System.out.println("Empty password field verification");
+            getDriver().findElement(By.id("password")).click();
+            getDriver().findElement(By.id("email")).click();
+            //Thread.sleep(2000);
+            WebElement emptyPasswordFieldNotificationElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(text(),'Please provide a password')]"))); //explicit wait
+            String emptyPasswordFieldNotification = emptyPasswordFieldNotificationElement.getAttribute("innerText").trim(); //explicit wait
+
+           // String emptyPasswordFieldNotification = getDriver().findElement(By.xpath("//*[contains(text(),'Please provide a password')]")).getAttribute("innerText").trim();
+            Assert.assertEquals(emptyPasswordFieldNotification, "Please provide a password.", "Password field does not have validation for empty value");
+
+        }
+
+        @Test(description = "Login - Negative Case - Login with invalid email and password fields")
+        public void loginNotRegisteredUserEmailFieldValidationNegativeCase() throws InterruptedException {
+
+            System.out.println("Invalid email field verification");
+            getDriver().findElement(By.id("email")).sendKeys(invalidUserName1);
+            getDriver().findElement(By.id("password")).sendKeys(password);
+            System.out.println("Clicking on Login button");
+            getDriver().findElement(By.id("loginButton")).click();
+           // Thread.sleep(2000);
+
+            WebElement invalidEmailNotificationElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(text(),'Invalid email or password.')]"))); //explicit wait
+            String invalidEmailNotification = invalidEmailNotificationElement.getAttribute("innerText").trim(); //explicit wait
+
+          //  String invalidEmailNotification = getDriver().findElement(By.xpath("//*[contains(text(),'Invalid email or password.')]")).getAttribute("innerText").trim();
+            Assert.assertEquals(invalidEmailNotification, "Invalid email or password.", "Email and password fields do not have validation for invalid values");
+
+        }
+
+    }
+
+
---
+++ src/test/java/io/ctdev/tests/login/ExplicitWait/RegisterUserJuiceShopTestPositiveExplicit.java
@@ -0,0 +1,131 @@
+package io.ctdev.tests.login.ExplicitWait;
+
+import io.ctdev.tests.framework.driver.WebDriverSingleton;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import static io.ctdev.tests.framework.driver.WebDriverSingleton.getDriver;
+
+public class RegisterUserJuiceShopTestPositiveExplicit {
+
+    WebDriver driver = getDriver(); //explicit wait
+    WebDriverWait wait; //explicit wait
+    private String password = "qQ2$4";
+    private String answer = "Coopert";
+
+    public String createRandomEmail() {
+        // create a string of all characters
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        // create random string builder
+        StringBuilder sb = new StringBuilder();
+
+        // create an object of Random class
+        Random random = new Random();
+
+        // specify length of random string
+        int length = 7;
+
+        for (int i = 0; i < length; i++) {
+
+            // generate random index number
+            int index = random.nextInt(alphabet.length());
+
+            // get character specified by index
+            // from the string
+            char randomChar = alphabet.charAt(index);
+
+            // append the character to string builder
+            sb.append(randomChar);
+        }
+        String randomString = sb.toString();
+        System.out.println("Random String is: " + randomString);
+        String randomEmail = randomString + "@gmail.com";
+        return randomEmail;
+    }
+
+    @BeforeClass
+    public void setUp() throws InterruptedException {
+        getDriver().get("http://3.18.213.48/");
+
+        getDriver().findElement(By.cssSelector("[class*='close-dialog']")).click();
+
+        System.out.println("Clicking on Account button");
+        WebElement element = getDriver().findElement(By.id("navbarAccount")); // можем либо создать объект элемента, либо напрямую его вызвать getDriver().
+        element.click();
+
+        System.out.println("Clicking on Login button");
+        getDriver().findElement(By.id("navbarLoginButton")).click();
+
+        System.out.println("Clicking 'Not yet a customer?' link");
+        getDriver().findElement(By.xpath("//*[contains(text(),'Not yet a customer?')]")).click(); //!!!!!
+
+        wait = new WebDriverWait(driver, 500); //explicit wait
+    }
+
+
+    @AfterClass
+    public void tearDown() {
+        WebDriverSingleton.closeDriver();
+    }
+
+    @Test(description = "Registration verification - Positive")
+    public void userRegistrationPositiveCase() throws InterruptedException {
+        String userEmail = createRandomEmail();
+
+        System.out.println("Typing data to email field" + userEmail);
+        getDriver().findElement(By.id("emailControl")).sendKeys(userEmail);
+
+        System.out.println("Typing data to password field");
+        getDriver().findElement(By.id("passwordControl")).sendKeys(password);
+
+        System.out.println("Typing data to Repeat Password field");
+        getDriver().findElement(By.id("repeatPasswordControl")).sendKeys(password);
+
+        System.out.println("Selecting data to Security Question field");
+
+        getDriver().findElement(By.xpath("//*[contains(@class,'mat-form-field-flex ng-tns-c123-16')]")).click();
+
+        // getDriver().findElement(By.xpath("//*[contains(text(),\"Mother's maiden name?\")]")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(text(),\"Mother's maiden name?\")]"))).click(); //explicit wait
+
+        System.out.println("Typing data to Security Answer field");
+        getDriver().findElement(By.id("securityAnswerControl")).sendKeys(answer);
+
+        System.out.println("Clicking Register button");
+        getDriver().findElement(By.id("registerButton")).click();
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(text(),'Not yet a customer?')]"))); //explicit wait
+
+        System.out.println("Typing user email" + userEmail);
+        getDriver().findElement(By.id("email")).sendKeys(userEmail);
+
+        System.out.println("Typing user password" + password);
+        getDriver().findElement(By.id("password")).sendKeys(password);
+
+        System.out.println("Clicking on Login button");
+        getDriver().findElement(By.id("loginButton")).click();
+
+        System.out.println("Check if user is logged in. Clicking on Account button.");
+        getDriver().findElement(By.id("navbarAccount")).click();
+
+        System.out.println("Getting user name from navigation bar");
+        //Thread.sleep(5000); //добавили ожидание, тк тесты падают из-за того что не успевает прогрузиться элемент, но не советую такое делать без крайней необходимости
+
+        WebElement actualUserNameElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[aria-label='Go to user profile'] span"))); //explicit wait
+        String actualUserName = actualUserNameElement.getAttribute("innerText").trim(); //explicit wait
+
+        Assert.assertEquals(actualUserName, userEmail, "User name does not match");
+    }
+
+}
